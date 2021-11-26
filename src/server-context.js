a70c28db17d248656319eca0a73d2280d9ea1a2e
Aleksey Kouzmenko <iskeyiv@gmail.com>
2021-11-26 11:24:07 +0400

    feat: add server context

---
+++ src/server-context.js
@@ -0,0 +1,27 @@
+import { Controller } from "./controller.js";
+import { PersonRepository } from "./person/person.repository.js";
+import { PersonService } from "./person/person.service.js";
+import { PersonController } from "./person/person.controller.js";
+import { ErrorHandler } from "./share/error-handler.js";
+
+export class ServerContext {
+    static getController() {
+        return new Controller(this.getPersonController());
+    }
+
+    static getErrorHandler() {
+        return new ErrorHandler();
+    }
+
+    static getPersonController() {
+        return new PersonController(this.getPersonService());
+    }
+
+    static getPersonService() {
+        return new PersonService(this.getPersonRepository());
+    }
+
+    static getPersonRepository() {
+        return new PersonRepository([]);
+    }
+}
