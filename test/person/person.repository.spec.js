e828c7cbfdd21730c2bf8c7e21d194a48a296723
Aleksey Kouzmenko <iskeyiv@gmail.com>
2021-11-26 16:32:00 +0400

    test: add repo tests

--- test/person/person.repository.spec.js
+++ test/person/person.repository.spec.js
@@ -36,4 +36,57 @@ describe("PersonRepository", () => {
             expect(() => repository.getPerson(123)).toThrow("Person with id 123 does not exist");
         });
     })
+
+    it("should add a person to the repository", () => {
+        const person = TestFactory.personWithoutUuidFrom("Tom", 15, [])
+        const repository = new PersonRepository([]);
+
+        const savedPerson = repository.addPerson(person);
+
+        expect(savedPerson).toBeDefined();
+        expect(savedPerson.name).toBe("Tom");
+        expect(repository.getPersons()[0].age).toBe(15);
+    });
+
+    describe("update person", () => {
+        it("should update and return updated person", () => {
+            const person = TestFactory.personFrom("Tom", 15, [])
+            const repository = new PersonRepository([person]);
+            const changedPerson = TestFactory.personWithUuidFrom(person.id, "Bob", 16, ["jogging"]);
+
+            const updatedPerson = repository.updatePerson(changedPerson);
+
+            expect(updatedPerson).toBeDefined();
+            expect(updatedPerson.id).toBe(person.id);
+            expect(updatedPerson.name).toBe("Bob");
+            expect(repository.getPersons()[0].age).toBe(16);
+        });
+
+        it("should throw an error", () => {
+            const repository = new PersonRepository([]);
+            const changedPerson = TestFactory.personFrom("Tom", 15, [])
+
+            expect(() => repository.updatePerson(changedPerson))
+                .toThrow(`Person with id ${changedPerson.id} does not exist`);
+        });
+    });
+
+    describe("delete person", () => {
+        it("should delete the person", () => {
+            const person = TestFactory.personFrom("Tom", 15, [])
+            const repository = new PersonRepository([person]);
+
+            repository.deletePerson(person.id);
+
+            expect(repository.getPersons()).toEqual([]);
+        });
+
+        it("should throw an error", () => {
+            const repository = new PersonRepository([]);
+            const id = TestFactory.anyUuid();
+
+            expect(() => repository.deletePerson(id))
+                .toThrow(`Person with id ${id} does not exist`);
+        });
+    })
 });
